dace5f0970044221fc0c54ef193c1d742b98bd10
John Safranek <john@wolfssl.com>
2017-02-02 13:50:20 -0800

    add header file

---
+++ wolfcast.h
@@ -0,0 +1,11 @@
+int
+WolfcastClient(
+    unsigned short myId,
+    const unsigned short *peerIdList,
+    unsigned int peerIdListSz);
+
+int
+WolfcastServer(
+    unsigned short myId,
+    const unsigned short *ignore1,
+    unsigned int ignore2);
